17469e2e98440778d39c207cc344b71d4a519c2c
Scholege <batuhan@scholege.com>
2026-01-07 15:56:02 +0300

    feat: button component finish

--- src/features/dashboard/pages/DashboardPage.tsx
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -3,13 +3,18 @@ import Button from '@/components/ui/Button';
 const DashboardPage = () => {
   return (
     <div>
-      <Button color="primary" size="large" leftIcon={<span>🔑</span>} isLoading>
+      <Button color="primary" size="large" isLoading={true}>
         Go to Login
       </Button>
-      <Button color="secondary" size="medium" leftIcon={<span>🔐</span>}>
+      <Button
+        color="secondary"
+        size="medium"
+        isLoading={false}
+        leftIcon={<span>🔐</span>}
+      >
         Go to Signup
       </Button>
-      <Button color="success" size="small" to="/dashboard" rightIcon={<span>➡️</span>}>
+      <Button color="success" size="medium" to="/dashboard" rightIcon={<span>➡️</span>}>
         Go to Dashboard
       </Button>
       <Button color="primary" size="small" fullWidth>
